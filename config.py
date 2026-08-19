5c0d897c690f54a5ede97bdb200f7192da86993b
Azamat E. Zakirovich <azamateshqoziyev2@gmail.com>
2026-08-19 21:53:19 +0500

    i stored dataset  and augmentation settings variables.

---
+++ config.py
@@ -0,0 +1,9 @@
+# dataset paths
+dataset_path = "./data"
+
+# augmentation settings
+image_size = 128
+amount_of_flip = 0.5
+angle_range = 15
+mean = [0.485, 0.456, 0.406]
+std = [0.229, 0.224, 0.225]
