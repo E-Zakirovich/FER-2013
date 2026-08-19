6b65139710381c16d98484596f2462f1b15ecbcc
Azamat E. Zakirovich <azamateshqoziyev2@gmail.com>
2026-08-19 21:51:06 +0500

    i made a transform for train dataset.

--- dataset.py
+++ dataset.py
@@ -17,9 +17,42 @@ It will do following operations:
 import torch
 from torch.utils.data import Subset, DataLoader, random_split
 from torchvision import datasets, transforms
-import config
+from config import image_size, amount_of_flip, angle_range, mean, std
 
 
 class LoadDataset:
     def __init__(self):
-        ...
+
+        # make a transform for train data
+        self.transform_for_train = transform.Compose([
+            # resize the image size
+            transforms.Resize(
+                # images size coming from config.py file
+                (
+                    image_size,
+                    image_size,
+                )
+            ),
+
+            # flipping the half of the data to avoid underfitting or overfitting
+            transforms.RandomHorizontalFlip(
+                p = amount_of_flip # amount_of_flip is coming from config.py file
+            ),
+
+            # rotate the image between -angle_range : angle_range to avoid underfitting or overfitting
+            transforms.RandomRotation(
+                p = angle_range # angle_range is coming from config.py file
+            ),
+
+            # make a tensor from images to fit the data to convolutional neural networks.
+            transforms.ToTensor(),
+
+            # normalization part
+            transforms.Normalize(
+                mean = mean, # mean is coming from config.py file
+                std = std, # std is coming from config.py file
+            )
+
+        ])
+
+        
