ce124cb45663addc4f7b334af1e5b23f4ea34a04
Azamat E. Zakirovich <azamateshqoziyev2@gmail.com>
2026-08-20 21:18:33 +0500

    i made a mistaker, instead of calling nnet methods with neuralnetworks.sth i called them nn.sth. It was very big bug. Also i imported torch in wrong way. It was torcn in stead of torch. and some bugs inside of comments. all of em fixed.

--- model/cnn.py
+++ model/cnn.py
@@ -7,7 +7,7 @@ It will use data pipeline and trained on train.py
 file.
 """
 
-import torcn.nn as neuralnetwork
+import torch.nn as neuralnetwork
 from config import in_out_channels, kernel_size, stride, padding, kernel_and_stride_size_for_pooling, input_layer, hidden_layer, output_layer, dropout
 
 
@@ -16,7 +16,7 @@ class CNN(neuralnetwork.NeuralNetwork):
         super(CNN, self).__init__()
 
         # first convolutional layer
-        self.first_convolutional_layer = nn.Conv2d(
+        self.first_convolutional_layer = neuralnetwork.Conv2d(
             in_channels = in_out_channels[0], # in channels (3)
             out_channels = in_out_channels[1], # out channels (32)
             kernel_size = kernel_size, # the size of kernel which is 3
@@ -24,12 +24,12 @@ class CNN(neuralnetwork.NeuralNetwork):
             padding = padding, # extra +1 size for top, down right and left
         )
         # first batch normalization
-        self.first_batch_normalization = nn.BatchNorm2d(
+        self.first_batch_normalization = neuralnetwork.BatchNorm2d(
             in_out_channels[1] # batch normalizer value which is 32 (help me to decreases number to make easy to train)
         )
 
         # second convolutional layer
-        self.second_convolutional_layer = nn.Conv2d(
+        self.second_convolutional_layer = neuralnetwork.Conv2d(
             in_channels = in_out_channels[1],  # in channels (32)
             out_channels = in_out_channels[2], # out channels (64)
             kernel_size = kernel_size, # the size of kernel which is 3
@@ -37,12 +37,12 @@ class CNN(neuralnetwork.NeuralNetwork):
             padding = padding, # extra +1 size for top, down right and left
         )
         # second batch normalization
-        self.second_batch_normalization = nn.BatchNorm2d(
+        self.second_batch_normalization = neuralnetwork.BatchNorm2d(
             in_out_channels[2] # batch normalizer value which is 64 (help me to decreases number to make easy to train)
         )
 
         # third convolutional layer
-        self.third_convolutional_layer = nn.Conv2d(
+        self.third_convolutional_layer = neuralnetwork.Conv2d(
             in_channels = in_out_channels[2],  # in channels (64)
             out_channels = in_out_channels[3],  # out channels (128)
             kernel_size = kernel_size,  # the size of kernel which is 3
@@ -50,7 +50,7 @@ class CNN(neuralnetwork.NeuralNetwork):
             padding = padding,  # extra +1 size for top, down right and left
         )
         # third batch normalization
-        self.third_batch_normalization = nn.BatchNorm2d(
+        self.third_batch_normalization = neuralnetwork.BatchNorm2d(
             in_out_channels[3]  # batch normalizer value which is 128 (help me to decreases number to make easy to train)
         )
 
@@ -58,8 +58,8 @@ class CNN(neuralnetwork.NeuralNetwork):
         self.relu = neuralnetwork.ReLU()
 
         # pooling
-        self.pool = neuralnetwork.MaxPool2D(
-            kernel_size = kernel_and_stride_size_for_pooling, # kernel size must be 2 in order to decrease the size of the image 3x
+        self.pool = neuralnetwork.MaxPool2d(
+            kernel_size = kernel_and_stride_size_for_pooling, # kernel size must be 2 in order to decrease the size of the image 2x
             stride = kernel_and_stride_size_for_pooling, # stride also must be 2 in order to decrease the size 2x times
         )
 
