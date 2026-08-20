cc2f2c803ec298e1ceccea3f404ebaaf841d1463
Azamat E. Zakirovich <azamateshqoziyev2@gmail.com>
2026-08-20 21:41:27 +0500

    forward propagation part is finished

--- model/cnn.py
+++ model/cnn.py
@@ -17,7 +17,7 @@ class CNN(neuralnetwork.Module):
 
         # first convolutional layer
         self.first_convolutional_layer = neuralnetwork.Conv2d(
-            in_channels = in_out_channels[0], # in channels (3)
+            in_channels = in_out_channels[0], # in channels (1)
             out_channels = in_out_channels[1], # out channels (32)
             kernel_size = kernel_size, # the size of kernel which is 3
             stride = stride, # the size of stride which is 1, it is amount of movement of kernel
@@ -81,3 +81,60 @@ class CNN(neuralnetwork.Module):
             hidden_layer, # hidden layer, size is 512 (my decision)
             output_layer # output layer, total is 7 because 7 moods we got.
         )
+
+    # forward propagation
+    def forward(self, x):
+
+        # first cycle for convolutional neural network
+        x = self.pool( # pooling (size is decreased by 2x)
+            self.relu( # get only positive values
+                self.first_batch_normalization( # normalize the values to make easy to train
+                    self.first_convolutional_layer( # first convolutional layer
+                        x # value
+                    )
+                )
+            )
+        )
+
+        # second cycle for convolutional neural network
+        x = self.pool(  # pooling (size is decreased by 2x)
+            self.relu(  # get only positive values
+                self.second_batch_normalization(  # normalize the values to make easy to train
+                    self.second_convolutional_layer(  # second convolutional layer
+                        x  # value
+                    )
+                )
+            )
+        )
+
+        # third cycle for convolutional neural network
+        x = self.pool(  # pooling (size is decreased by 2x)
+            self.relu(  # get only positive values
+                self.third_batch_normalization(  # normalize the values to make easy to train
+                    self.third_convolutional_layer(  # third convolutional layer
+                        x  # value
+                    )
+                )
+            )
+        )
+
+        x = x.view(x.size(0), -1)
+
+        # connection between input layer and hidden layer
+        x = self.relu( # get only positive values
+            self.input_layer_and_hidden_layer_connection( # connection part
+                x # values
+            )
+        )
+
+        # drop 30% of weights
+        x = self.dropout(
+            x
+        )
+
+        # connection between hidden layer and output layer
+        x = self.hidden_layer_and_output_layer_connection(
+                x
+            )
+
+        return x # return the result
