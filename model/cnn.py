d726ccc4985c37054592f194ec92d8dd66e13844
Azamat E. Zakirovich <azamateshqoziyev2@gmail.com>
2026-08-20 20:36:03 +0500

    first convolutional layer and batch normalizer is finished.

--- model/cnn.py
+++ model/cnn.py
@@ -8,9 +8,22 @@ file.
 """
 
 import torcn.nn as neuralnetwork
-from config import *
+from config import in_out_channels, kernel_size, stride, padding
 
 
 class CNN(neuralnetwork.NeuralNetwork):
     def __init__(self):
         super(CNN, self).__init__()
+
+        # first convolutional layer
+        self.first_convolutional_layer = nn.Conv2d(
+            in_channels = in_out_channels[0], # in channels (3)
+            out_channels = in_out_channels[1], # out channels (32)
+            kernel_size = kernel_size,
+            stride = stride,
+            padding = padding,
+        )
+        # first batch normalization
+        self.first_batch_normalization = nn.BatchNorm2d(
+            in_out_channels[1] 
+        )
