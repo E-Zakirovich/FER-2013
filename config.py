3d12592061ab62367e7f03fc9aa68981e51a3a35
Azamat E. Zakirovich <azamateshqoziyev2@gmail.com>
2026-08-20 21:41:53 +0500

    new params for convolutional netural netoworks.

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 # dataset paths
 dataset_path = "./data"
 
-# augmentation settings
+# data pipeline settings
 image_size = 48
 amount_of_flip = 0.5
 angle_range = 15
@@ -14,3 +14,14 @@ train_split = 0.8
 validation_split = 0.1
 test_split = 0.1
 total_channels = 1
+
+# convolutional neural networks parameters
+in_out_channels = [1, 32, 64, 128]
+kernel_size = 3
+stride = 1
+padding = 1
+kernel_and_stride_size_for_pooling = 2
+input_layer = 6 * 6 * 128
+hidden_layer = 512
+output_layer = 7
+dropout = 0.3
