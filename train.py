547c49d4ca3a92c86c09feb60075de9fcc4b688f
Azamat E. Zakirovich <azamateshqoziyev2@gmail.com>
2026-08-21 22:16:31 +0500

    fit method is finished, and now it is time to write evaluator.

--- train.py
+++ train.py
@@ -10,7 +10,7 @@ pipeline. So it is time to train them in main.py
 import torch
 import torch.optim as optimize
 import torch.nn as neuralnetwork
-from config import learning_rate
+from config import learning_rate, total_channels
 
 
 class Train:
@@ -36,18 +36,39 @@ class Train:
 
     # following method will help me to train
     def fit(self, epochs):
-        # loop for epoch
 
-            # train the model
+        for epoch in range(epochs): # I need a loop to train epochs times
 
-            # running, training loss and total corercts
+            self.model.train() # train the model
 
-            # loop to see images & labels
+            # I need to measure train loss, running loss and correct values for evaluation part
 
-                # optmize zero grad
-                # get the output from model
-                # find loss function
-                # optimizer
+            running_loss = 0.0
+            correct = 0
+            total = 0
+
+            for images, labels in self.train_data: # I need a loop to try each train images
+
+                images = images.to(self.device) # I am loading images to hardware
+                labels = labels.to(self.device) # I am loading labels to hardware
+
+                self.optimizer.zero_grad() # it is better to make zero grad before get the output from convolutional neural network
+
+                output = self.model(images) # I get the output from convolutional neural network
+
+                loss = self.criterion( # find loss function
+                    output, # output from convolutional neural network
+                    labels # labels to check the correctness of output
+                )
+
+                loss.backward()  # backpropagation algorithm is working here 
+
+                self.optimizer.step() # after calculation of loss function better optimize weights. Actually, I used ADAM to optimize
 
                 # running loss, total corrects, total, corrects
-        ...
+                running_loss += loss.item() * labels.size(0)
+                _, predicted = torch.max(output.data, 1)
+                correct += (predicted == labels).sum().item()
+                total += labels.size(0)
+
+            # evaluation part
