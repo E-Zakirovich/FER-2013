0681a16e1c1071e6201beae0492e25d774424461
Azamat E. Zakirovich <azamateshqoziyev2@gmail.com>
2026-08-21 21:38:29 +0500

    all initializers, loss functions and optimization functions are written.

--- train.py
+++ train.py
@@ -10,9 +10,26 @@ pipeline. So it is time to train them in main.py
 import torch
 import torch.optim as optimize
 import torch.nn as neuralnetwork
+from config import learning_rate
 
 
 class Train:
 
-    def __init__(self):
-        ...
+    def __init__(
+            self,
+            model, # my convolutional neural network is coming from here
+            device, # device, cpu or GPU
+            train_loader, # train dataset is coming here
+            validation_loader, # validation dataset is coming here
+    ):
+        self.device = device
+        self.model = model.to(device)
+        self.train_data = train_loader
+        self.validation_data = validation_loader
+        
+        # optimizer part
+        self.criterion = neuralnetwork.CrossEntropyLoss() # cost function or loss function
+        self.optimizer = optimize.Adam( # I am using ADAM optimizer for optimization
+            self.model.parameters(), # parameters of my model
+            lr=learning_rate, # learning rate
+        )
