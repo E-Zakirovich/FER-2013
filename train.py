4bcf1288789d13f79aaf8883b1036f9bc19b81c9
Azamat E. Zakirovich <azamateshqoziyev2@gmail.com>
2026-08-21 16:12:26 +0500

    train.py file explanation is written, packages are imported and class is ready to write a code.

---
+++ train.py
@@ -0,0 +1,18 @@
+"""
+train.py
+~~~~~~~~~
+
+I created a convolutional neural networks and data
+pipeline. So it is time to train them in main.py
+"""
+
+# load packages
+import torch
+import torch.optim as optimize
+import torch.nn as neuralnetwork
+
+
+class Train:
+
+    def __init__(self):
+        ...
